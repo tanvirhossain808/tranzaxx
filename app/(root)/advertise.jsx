a7c5cf11f35e35c62a8febcee955222612eb4f8d
Md. Tanvir Hossain <151984579+tanvirhossain808@users.noreply.github.com>
2024-11-02 02:12:35 +0600

    fix margin issue

--- app/(root)/advertise.jsx
+++ app/(root)/advertise.jsx
@@ -7,28 +7,30 @@ import CardInformation from "../../components/advertise/CardInformation"
 const advertise = () => {
     return (
         <ScrollView className="bg-white min-h-screen">
-            <Hero />
-            <View className="mt-6 px-4">
-                <PersonalInformation />
-                <CardHolderInformationSection />
-                <CardInformation />
+            <View className="mb-20">
+                <Hero />
+                <View className="mt-6 px-4">
+                    <PersonalInformation />
+                    <CardHolderInformationSection />
+                    <CardInformation />
 
-                <View className="mt-[26px] mb-20">
-                    <Text className="text-primary font-poppins text-[18px] text-right font-semibold mt-2">
-                        Sub total 1000$
-                    </Text>
-                    <Text className="text-primary font-poppins text-[18px] text-right font- mt-2">
-                        Discount os
-                    </Text>
-                    <Text className="text-primary font-poppins text-[18px] text-right font-semibold mt-2">
-                        total price 1000s
-                    </Text>
-                    <View className="mt-1 flex-row items-center justify-end">
-                        <TouchableOpacity className="bg-[#00ADEF] w-[168px] p-[10px] rounded-[12px]">
-                            <Text className="font-poppins text-[18px] text-white uppercase font-semibold text-center">
-                                Pay now
-                            </Text>
-                        </TouchableOpacity>
+                    <View className="mt-[26px] mb-20">
+                        <Text className="text-primary font-poppins text-[18px] text-right font-semibold mt-2">
+                            Sub total 1000$
+                        </Text>
+                        <Text className="text-primary font-poppins text-[18px] text-right font- mt-2">
+                            Discount os
+                        </Text>
+                        <Text className="text-primary font-poppins text-[18px] text-right font-semibold mt-2">
+                            total price 1000s
+                        </Text>
+                        <View className="mt-1 flex-row items-center justify-end">
+                            <TouchableOpacity className="bg-[#00ADEF] w-[168px] p-[10px] rounded-[12px]">
+                                <Text className="font-poppins text-[18px] text-white uppercase font-semibold text-center">
+                                    Pay now
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
             </View>
